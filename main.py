02a9476ddd2f3c07dedc02ce1b802c1501211806
Ashley Conner <108750120+valardohaeris2@users.noreply.github.com>
2025-01-04 10:06:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ STOCK_ENDPOINT = "https://www.alphavantage.co/query/"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"  
   
 STOCK_API_KEY = ""    
-NEWS_API_KEY = "" 
+NEWS_API_KEY = ""  
 TWILIO_SID = "" 
 TWILIO_AUTH_TOKEN = ""    
  
